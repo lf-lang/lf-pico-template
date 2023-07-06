83f4f3ac19929c554d4cc32b1f3af793695f2293
Abhinav Gundrala <gundralaa@berkeley.edu>
2023-07-06 14:47:27 -0700

    initial build setup

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # lf-pico-template
 Template for Lingua Franca projects using the baremetal RP2040 target platform
+
+Use lingo to build different subsets of applications
+- will copy all binaries to to the top level bin directory
+- the node testrunner can be invoked. this will look in the bin directory, load and run each test hex
+- these will be either in a test framework with different cases and asserts that look at the entire uart output
+- 
---
+++ shell.nix
@@ -0,0 +1,34 @@
+{ pkgs ? import <nixpkgs> {} } :
+let
+  # lf = pkgs.callPackage ./nix/lf.nix {};
+  # lingo = pkgs.callPackage ./nix/lingo.nix {};
+in 
+pkgs.mkShell {
+  packages = with pkgs; [
+    picotool
+    cmake
+    gcc-arm-embedded
+    openocd
+    git
+    nodejs
+    zellij
+  ];
+  buildInputs = [
+    # lf
+    # lingo
+  ];
+
+  shellHook = ''
+    echo "[shell] hook start"
+    echo "[shell] setup pico-sdk"
+    git submodule update --init
+    cd pico-sdk/
+    git submodule update --init
+    export PICO_SDK_PATH="$PWD"
+    cd ../
+    echo "[shell] setup testbed"
+    cd test/
+    npm install
+    echo "[shell] hook end"
+    '';
+}
